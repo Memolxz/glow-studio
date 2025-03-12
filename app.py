4618a6fd4133fc58e6756d9996049c188cfd049a
Memolxz <bruhbummer@gmail.com>
2025-03-12 11:22:32 -0300

    base flask

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        return render_template('index.html')
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
