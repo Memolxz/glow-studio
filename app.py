3b70ee7282bb5f8771d8267a6d9b7586e229b811
Memolxz <bruhbummer@gmail.com>
2025-03-15 08:44:18 -0300

    agregue productos

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def home():
     if request.method=='POST':
-        return render_template('index.html')
+        return redirect(url_for('products'))
     
     return render_template('index.html')
+
+@app.route('/products',methods=['GET','POST'])
+def products():
+    if request.method=='POST':
+        return redirect(url_for('home'))
+    
+    return render_template('products.html')
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
