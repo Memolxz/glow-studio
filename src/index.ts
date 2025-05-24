28c95cf98c06a66d4c9a86faf6395844de6c84ce
LuStMartin03 <lsaintmartin@alumno.huergo.edu.ar>
2025-05-24 14:53:10 +0000

    skyn type y user skyntype router y services

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,16 @@ import "dotenv/config"
 import express from 'express';
 
 import { userRouter } from './routers/userRouter';
+import { userSkinTypeRouter } from './routers/userSkinTypeRouter';
+import { skinTypeRouter } from './routers/skinTypeRouter';
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/users', userRouter)
+app.use('/skinType', skinTypeRouter)
+app.use('/userSkinType', userSkinTypeRouter)
 
 app.listen(8000, () => {
   console.log(`App listening on http://localhost:8000`)
