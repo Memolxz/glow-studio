4427e32f229c409c364be9e9f507b9b6874711bd
LuStMartin03 <lsaintmartin@alumno.huergo.edu.ar>
2025-08-10 22:41:10 -0300

    header

--- apps/ui/src/components/Header.tsx
+++ apps/ui/src/components/Header.tsx
@@ -1,24 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
         <header className="w-full absolute top-14 left-0 z-50 bg-transparent">
         <div className="max-w-[1100px] mx-auto py-3 flex items-center font-inter">
 
             <nav className="flex gap-12 flex-1 text-sm font-semibold text-white tracking-wide items-center justify-start">
-            <a href="#" className="hover:text-gray-200 transition-colors">PRODUCTOS</a>
-            <a href="#" className="hover:text-gray-200 transition-colors">RUTINA</a>
-            <a href="#" className="hover:text-gray-200 transition-colors">CONTACTO</a>
+            <Link to={"/products"} className="hover:text-gray-200 transition-colors">PRODUCTOS</Link>
+            <Link to={"/routine"} className="hover:text-gray-200 transition-colors">RUTINA</Link>
             </nav>
-
 
             <h1 className="flex-1 text-center text-2xl font-bold tracking-wide text-white">
             GLOW STUDIO
             </h1>
 
-
             <nav className="flex gap-8 flex-1 justify-end text-sm font-semibold text-white tracking-wide items-center">
-            <a href="#" className="border border-white text-sm py-1 px-5 rounded-full hover:bg-white hover:text-black transition-colors">
+            <Link to={"/register"} className="border border-white text-sm py-1 px-5 rounded-full hover:bg-white hover:text-black transition-colors">
                 LOG IN
-            </a>
+            </Link>
             </nav>
         </div>
         </header>
