d4cd8bf63c9a33bd43f61725ffbe0e3b7d439afc
LuStMartin03 <lsaintmartin@alumno.huergo.edu.ar>
2025-08-11 16:30:55 -0300

    app

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,22 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
+import SkinSelection from "./pages/Selection"
 import AuthLayout from "./pages/AuthPage"
 import Products from "./pages/Products"
 import Profile from "./pages/Profile"
 import Routine from "./pages/Routine"
 import Home from "./pages/Home"
 import FQ from "./pages/FAQ"
-import SkinSelection from "./pages/Selection"
 
 export default function App() {
     return (
     <Routes>
+      <Route path="/selection" element={<SkinSelection />} />
       <Route path="/register" element={<AuthLayout />} />
       <Route path="/products" element={<Products />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/routine" element={<Routine />} />
       <Route path="/home" element={<Home />} />
       <Route path="/faq" element={<FQ />} />
-      <Route path="/selection" element={<SkinSelection />} />
     </Routes>
     );
 }
