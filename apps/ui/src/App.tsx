3325e12bc9c600486891ba899411b586e86eb022
Memolxz <112393745+Memolxz@users.noreply.github.com>
2025-09-27 12:11:08 +0000

    modig  skintype and drawback auth

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,8 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import AuthGuard from './components/AuthGuard';
-// import ProtectedRoute from './components/ProtectedRoute';
-// import AuthRedirect from './components/AuthRedirect';
-
 import SkinSelection from "./pages/Selection"
 import AuthLayout from "./pages/AuthPage"
 import Products from "./pages/Products"
@@ -13,19 +9,17 @@ import FQ from "./pages/FAQ"
 import RecommendationsPage from "./pages/Recommendations"
 
 export default function App() {
-    return (
-    <Routes>
-      <AuthGuard>
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/selection" element={<SkinSelection />} />
-        <Route path="/register" element={<AuthLayout />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/routine" element={<Routine />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/faq" element={<FQ />} />
-        <Route path="/recommendations" element={<RecommendationsPage />} />
-      </AuthGuard>
-    </Routes>
+    return (   
+      <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/selection" element={<SkinSelection />} />
+          <Route path="/register" element={<AuthLayout />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/routine" element={<Routine />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/faq" element={<FQ />} />
+          <Route path="/recommendations" element={<RecommendationsPage />} />
+      </Routes>
     );
 }
