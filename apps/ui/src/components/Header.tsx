a1fe65dbadaf49c8456d802abaa59ec795553b19
LuStMartin03 <lsaintmartin@alumno.huergo.edu.ar>
2025-10-04 12:14:11 -0300

    boton de cerrar sesion

--- apps/ui/src/components/Header.tsx
+++ apps/ui/src/components/Header.tsx
@@ -5,6 +5,7 @@ import { Menu, X, UserRoundIcon, LogOut } from "lucide-react";
 export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isOptionsOpen, setIsOptionsOpen] = useState(false);
+    const [isOptionSignOutOpen, setIsOptionSignOutOpen] = useState(false);
 
     return (
         <header className="w-full absolute top-14 z-50 bg-transparent">
@@ -63,7 +64,6 @@ export default function Header() {
             {isMenuOpen && (
             <div
                 className="fixed inset-0 bg-black bg-opacity-50 z-40"
-                onClick={() => setIsMenuOpen(false)}
             ></div>)}
 
             </div>
@@ -78,35 +78,43 @@ export default function Header() {
                 <div className="relative">
                 <button
                     onClick={() => setIsOptionsOpen(!isOptionsOpen)}
-                    className="flex items-center justify-center border-2 h-10 w-10 border-white rounded-full hover:border-darkblue hover:bg-transparent hover:text-darkblue hover:scale-105 transition-transform"
+                    className="flex items-center justify-center border-2 h-10 w-10 border-white rounded-full hover:border-darkblue hover:bg-transparent hover:text-darkblue hover:scale-105 transition-transform z-30"
                 >
                     <UserRoundIcon className="scale-150" strokeWidth={1.5}/>
                 </button>
 
                 {isOptionsOpen && (
-                    <div className="absolute top-full right-0 mt-2 w-48 bg-background rounded-2xl shadow-lg flex flex-col text-start z-30">
+                    <div className="absolute top-full right-0 mt-2 w-48 bg-background rounded-2xl shadow-lg flex flex-col text-start z-30
+                                    origin-top transition-all duration-300 ease-out animate-[growDown_0.25s_ease-out]">
                         <Link
                             to={"/profile"}
                             onClick={() => setIsOptionsOpen(false)}
-                            className="flex flex-row py-2 mt-1 text-darkblue hover:text-hovertext text-lg transition-transform hover:scale-105 ml-3 origin-left"
-                        >
+                            className="flex flex-row py-2 mt-1 text-darkblue hover:text-hovertext text-lg transition-transform hover:scale-105 ml-3 origin-left z-50">
                             <UserRoundIcon className="mr-2"/>
                             <p>Perfil</p>
                         </Link>
                         <div className="mx-auto flex w-[90%] items-center border-t border-gray-500 rounded-full"/>
                         <button
-                            onClick={() => setIsOptionsOpen(false)}
+                            onClick={() => {
+                                    setIsOptionSignOutOpen(!isOptionSignOutOpen);
+                                    setIsOptionsOpen(!isOptionsOpen);
+                                    }}
                             className="flex flex-row py-2 mb-1 text-red-800 hover:text-red-900 text-lg transition-transform hover:scale-105 ml-3 origin-left"
                         >
                             <LogOut className="mr-2"/>
                             <p>Cerrar Sesión</p>
-                            
                         </button>
                     </div>
                 )}
+                {isOptionsOpen && (
+                    <div
+                        className="fixed inset-0 mt-28 z-10"
+                        onClick={() => setIsOptionsOpen(!isOptionsOpen)}>
+                    </div>)}
             </div>
 
-{/* 
+
+            {/* 
                 <Link
                     to={"/register"}
                     className="border border-white text-sm py-1 px-5 rounded-full hover:bg-white hover:text-darkblue hover:scale-105 transition-transform"
@@ -115,6 +123,36 @@ export default function Header() {
                 </Link> */}
             </nav>
         </div>
+            {isOptionSignOutOpen && (
+                <div className="flex flex-row justify-center items-center w-[80%] mx-auto py-3 mt-16">
+                    <div className="absolute top-full mt-2 w-1/3 bg-background rounded-2xl shadow-lg flex flex-col text-start z-30 origin-top">
+                        <h1 className="m-2 text-center font-inter text-xl">¿Estás seguro que querés cerrar sesión?</h1>
+                        <div className="flex w-full items-center border-t border-gray-500 rounded-full"/>
+                        <div className="flex flex-row justify-center items-center">
+                            <Link
+                                to={"/profile"}
+                                className="w-1/2 flex flex-row text-darkblue justify-center items-center hover:text-hovertext text-xl text-center
+                                            transition-transform hover:scale-105 origin-center">
+                                <p>Aceptar</p>
+                            </Link>
+                            <div className="h-10 border-l border-gray-500 rounded-full" />
+                            <button
+                                onClick={() => {
+                                    setIsOptionSignOutOpen(!isOptionSignOutOpen);
+                                    setIsOptionsOpen(!isOptionsOpen);
+                                    }}
+                                className="w-1/2 flex flex-row text-red-800 hover:text-red-900 text-xl text-center justify-center items-center
+                                            transition-transform hover:scale-105 origin-center">
+                                <p>Cancelar</p>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                )}
+                {isOptionSignOutOpen && (
+                    <div
+                        className="fixed inset-0 bg-black bg-opacity-50 z-10"
+                    ></div>)}
         </header>
     );
 }
