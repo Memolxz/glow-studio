80177a0bbb248f05fdafc44cb1d2812855fffdef
LuStMartin03 <lsaintmartin@alumno.huergo.edu.ar>
2025-10-16 19:33:57 -0300

    mezcla viejo - nuevo

--- apps/ui/src/pages/Products.tsx
+++ apps/ui/src/pages/Products.tsx
@@ -1,9 +1,110 @@
-import { useState, useEffect  } from "react";
+import { useState, useRef, useEffect } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import img1 from "../assets/modelo3.png";
-import { ChevronLeft, ChevronRight, SlidersHorizontal, Star, X } from "lucide-react";
+import { SlidersHorizontal, Star, X } from "lucide-react";
 import { Link } from "react-router-dom";
+
+function Title() {
+    const [isFiltersOpen, setIsFiltersOpen] = useState(false);
+    const filtersRef = useRef<HTMLDivElement | null>(null);
+    const toggleRef = useRef<HTMLButtonElement | null>(null);
+    const [price, setPrice] = useState(20000); // precio inicial
+
+    // Cierra al hacer click fuera
+    useEffect(() => {
+        function handleClickOutside(e: MouseEvent) {
+        const target = e.target as Node;
+        if (
+            isFiltersOpen &&
+            filtersRef.current &&
+            !filtersRef.current.contains(target) &&
+            toggleRef.current &&
+            !toggleRef.current.contains(target)
+        ) {
+            setIsFiltersOpen(false);
+        }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, [isFiltersOpen]);
+    return(
+    <div className="flex flex-col items-center justify-center w-full">
+        <div className="flex flex-row items-center justify-center w-[90%]">
+            <div className="flex flex-col items-start justify-start w-2/3">
+                <h1 className="text-4xl font-bold text-start text-darkblue">Productos</h1>
+                <p className="text-xl font-normal text-start text-darkblue/60">Descubre los productos que ofrecemos.</p>
+            </div>
+
+            <div className="flex flex-col items-end justify-center w-1/3 relative">
+            <div className="relative">
+                <button
+                ref={toggleRef}
+                className="flex flex-row items-center justify-center w-48 h-10 bg-darkblue hover:bg-hovertext text-white rounded-2xl"
+                onClick={() => setIsFiltersOpen((s) => !s)}
+                aria-haspopup="true"
+                aria-expanded={isFiltersOpen}
+                >
+                <h1 className="text-lg font-semibold text-start mr-2">Mostrar Filtros</h1>
+                <SlidersHorizontal className="h-6 w-6" />
+                </button>
+
+                {isFiltersOpen && (
+                <div
+                    ref={filtersRef}
+                    className="absolute right-0 mt-2 w-80 bg-white text-darkblue z-50 rounded-2xl p-4 shadow-lg">
+                    <div className="flex flex-col gap-2">
+
+                        <label className="font-semibold">Categoría</label>
+                        <select className="w-full rounded-md p-2 text-darkblue">
+                            <option value="">Todos</option>
+                            <option value="serum">Serum</option>
+                            <option value="tonificador">Tonificador</option>
+                            <option value="limpiador">Limpiador</option>
+                            <option value="exfoliante">Exfoliante</option>
+                            <option value="mascarilla">Mascarilla</option>
+                            <option value="protector solar">Protector Solar</option>
+                            <option value="tratamiento">Tratamiento</option>
+                            <option value="hidratante">Hidratante</option>
+                        </select>
+
+                        <label className="font-semibold">Marca</label>
+                        <select className="w-full rounded-md p-2 text-darkblue">
+                            <option value="">Todos</option>
+                            <option value="serum">Clinique</option>
+                            <option value="tonificador">La Roche Posay</option>
+                            <option value="limpiador">Dermaglós</option>
+                        </select>
+
+                        <div className="flex flex-col">
+                            <label className="font-semibold mb-2">Rango de precio</label>
+                            <div className="flex items-center justify-between w-full">
+                                <span className="text-sm font-medium">$0</span>
+
+                                <input
+                                    type="range"
+                                    min={0}
+                                    max={50000}
+                                    step={1000}
+                                    value={price}
+                                    onChange={(e) => setPrice(Number(e.target.value))}
+                                    className="w-3/5 accent-darkblue cursor-pointer"
+                                />
+
+                                <span className="text-sm font-medium">${price.toLocaleString()}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                )}
+            </div>
+            </div>
+        </div>
+
+        <div className="border-b border-darkblue/60 my-5 w-[90%]"></div>
+    </div>
+    );
+}
 
 type Product = {
     id: number;
@@ -18,14 +119,14 @@ type Product = {
 };
 
 const categoryDisplayNames: Record<string, string> = {
-  SERUM: "Sérum",
-  CLEANSER: "Limpiador",
-  TONER: "Tonificador",
-  SUNSCREEN: "Protector Solar",
-  MASK: "Mascarilla",
-  MOISTURIZER: "Hidratante",
-  EXFOLIANT: "Exfoliante",
-  TREATMENT: "Tratamiento",
+    SERUM: "Sérum",
+    CLEANSER: "Limpiador",
+    TONER: "Tonificador",
+    SUNSCREEN: "Protector Solar",
+    MASK: "Mascarilla",
+    MOISTURIZER: "Hidratante",
+    EXFOLIANT: "Exfoliante",
+    TREATMENT: "Tratamiento",
 };
 
 export default function Products() {
@@ -109,30 +210,57 @@ export default function Products() {
 
     if (loading) {
         return (
-        <div className="min-h-screen flex items-center justify-center bg-defaultbg">
-            <div className="text-center">
-            <div className="text-xl font-semibold text-darkblue mb-4">
-                Cargando productos...
+            <div className="bg-white relative font-inter">
+                <div className="flex flex-col items-center w-full">
+                    <Header />
+                    <div className="w-[90%] py-10 -mt-22">
+                        <div className="w-full sm:h-[300px] md:h-[400px] lg:h-[450px] overflow-hidden rounded-3xl relative">
+                            <img src={img1} alt="Rhode" className="w-full object-cover" />
+                        </div>
+                    </div>
+                    <Title />
+                    <div className="flex items-center justify-center bg-defaultbg w-full">
+                        <div className="text-center">
+                        <div className="text-xl font-semibold text-darkblue mb-4">
+                            Cargando productos...
+                        </div>
+                        <div className="w-8 h-8 border-4 border-darkblue border-t-transparent rounded-full animate-spin mx-auto"></div>
+                        </div>
+                    </div>
+
+                    <Footer />
+                </div>
             </div>
-            <div className="w-8 h-8 border-4 border-darkblue border-t-transparent rounded-full animate-spin mx-auto"></div>
-            </div>
-        </div>
         );
     }
 
     if (error) {
         return (
-        <div className="min-h-screen flex items-center justify-center bg-defaultbg">
-            <div className="text-center">
-            <div className="text-xl font-semibold text-red-600 mb-4">{error}</div>
-            <button
-                onClick={fetchProducts}
-                className="px-6 py-2 bg-darkblue text-white rounded-full hover:bg-hovertext transition"
-            >
-                Reintentar
-            </button>
+            <div className="bg-white relative font-inter">
+                <div className="flex flex-col items-center w-full">
+                    <Header />
+
+                    <div className="w-[90%] py-10 -mt-22">
+                        <div className="w-full sm:h-[300px] md:h-[400px] lg:h-[450px] overflow-hidden rounded-3xl relative">
+                            <img src={img1} alt="Rhode" className="w-full object-cover" />
+                        </div>
+                    </div>
+                    <Title />
+                    <div className="flex items-center justify-center bg-defaultbg">
+                    <div className="text-center">
+                        <div className="text-xl font-semibold text-red-600 mb-4">{error}</div>
+                        <button
+                            onClick={fetchProducts}
+                            className="px-6 py-2 bg-darkblue text-white rounded-full hover:bg-hovertext transition"
+                        >
+                            Reintentar
+                        </button>
+                        </div>
+                    </div>
+
+                    <Footer />
+                </div>
             </div>
-        </div>
         );
     }
 
@@ -502,4 +630,42 @@ export default function RecommendationsPage() {
 
     return <ProductRecommendations products={products} />;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div className="bg-white relative font-inter">
+            <div className="flex flex-col items-center">
+                <Header />
+                <div className="w-[90%] py-10 -mt-22">
+                <div className="w-full sm:h-[300px] md:h-[400px] lg:h-[450px] overflow-hidden rounded-3xl relative">
+                    <img src={img1} alt="Rhode" className="w-full object-cover" />
+                </div>
+                </div>
+
+                <Footer />
+            </div>
+        </div>
  */
